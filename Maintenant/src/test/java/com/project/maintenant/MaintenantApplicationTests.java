c5de543c46f39d893d695a1cf9ce4a13cda41aad
Gaurav Kumar <gaurav2697@gmail.com>
2022-04-09 14:00:29 +0530

    folder structure first commit

---
+++ Maintenant/src/test/java/com/project/maintenant/MaintenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.maintenant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
